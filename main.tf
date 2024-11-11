01ffa5137a4e1ce50341b3a2264efb80166020ab
antierisalu <antikaz@gmail.com>
2024-11-11 23:15:04 +0200

    instance with web access

--- main.tf
+++ main.tf
@@ -7,4 +7,16 @@ resource "aws_instance" "terraform_instance" {
     instance_type = "t2.micro"
     subnet_id = "subnet-07b6570bb4fb71ce8"
     key_name = "aws-keypair"
+    security_groups = ["sg-0b543a82a9477b0c9"]
+    associate_public_ip_address = true
 }
+
+# aws ec2 allocate-address
+
+# aws ec2 associate-address --insta
+# nce-id i-012302be6b6951c78 --allocation-id eipalloc-07e81fffb3d912389
+
+# aws ec2 authorize-security-group-
+# ingress --group-id sg-0b543a82a9477b0c9 --protocol tcp --port 22 --cidr 0.0.0
+# .0/0
+
